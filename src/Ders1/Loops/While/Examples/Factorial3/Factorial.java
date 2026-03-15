9c7679152f7ed87b43f1be741c576757be53c8b6
Akın Kazar <akinkazar9@gmail.com>
2026-03-15 20:50:31 +0300

    Factorial code added at 20.50

--- src/Ders1/Loops/While/Examples/Factorial3/Factorial.java
+++ src/Ders1/Loops/While/Examples/Factorial3/Factorial.java
@@ -1,5 +1,28 @@
 package Ders1.Loops.While.Examples.Factorial3;
 
+import java.util.Scanner;
+
 public class Factorial {
+    public static void main(String[] args) {
+
+        int number, tempNumber;
+        int toplam = 1;
+
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Bir Sayı Gir: ");
+        number = input.nextInt();
+
+        tempNumber = number;
+        while (1 <= tempNumber) {
+
+            toplam *= tempNumber;
+            tempNumber--;
+
+        }
+        System.out.println(number + "! = " + toplam);
+
+    }
 
 }
--- src/Ders1/Loops/While/Examples/PowersOf2/PowersOfTwo.java
+++ src/Ders1/Loops/While/Examples/PowersOf2/PowersOfTwo.java
@@ -1,4 +1,25 @@
 package Ders1.Loops.While.Examples.PowersOf2;
 
+import java.util.Scanner;
+
 public class PowersOfTwo {
+    public static void main(String[] args) {
+
+        int number;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Bir Sayı Gir: ");
+        number = input.nextInt();
+
+        int i = 1;
+
+        while (i <= number) {
+
+            System.out.println(i);
+            i *= 2;
+
+        }
+
+    }
 }
